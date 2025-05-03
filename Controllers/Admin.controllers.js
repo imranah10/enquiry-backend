8209fbd4da3ca955ed811f3cc41fb18494261115
imranah10 <imranaha310@gmail.com>
2025-05-03 20:45:12 +0530

    code impove

--- Controllers/Admin.controllers.js
+++ Controllers/Admin.controllers.js
@@ -5,11 +5,11 @@ dotenv.config();
 export const loginAdmin = (req, res) => {
   const { username, password } = req.body;
 
-  const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
-  const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+  const ADMIN_USERNAME =  process.env.ADMIN_USERNAME;
+  const ADMIN_PASSWORD =  process.env.ADMIN_PASSWORD;
 
   if (username === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
-    return res.status(200).json({ username,message: 'Login successful' });
+    return res.status(200).json({ username,message: 'Login successful'  });
   } else {
     return res.status(401).json({ error: 'Invalid credentials' });
   }
